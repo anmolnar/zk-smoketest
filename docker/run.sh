f9f1c128c6ada924d89b4ab7cfd8dc0ad1fed570
Andor Molnar <andor@cloudera.com>
2026-09-09 12:36:09 -0500

    Upgrade to Debian 13

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+set -e -x -u
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
 IMAGE_NAME=andor/zktest
-ZOOKEEPER_ROOT=/Users/andor/tmp/apache-zookeeper-3.8.6
+ZOOKEEPER_ROOT=/Users/andor/tmp/apache-zookeeper-3.9.6
+
+pushd "$SCRIPT_DIR"
 
 echo "Building Docker image: ${IMAGE_NAME}"
 docker build --rm=true -t "${IMAGE_NAME}" ./
+
+popd
 
 if [ $? -ne 0 ]; then
     echo "Error: Failed to build ZooKeeper Docker image."
@@ -15,6 +23,7 @@ fi
 
 docker run -i -t \
   --rm=true \
+  -v ~/.m2:/root/.m2 \
   -v "$ZOOKEEPER_ROOT":/zookeeper \
   "$IMAGE_NAME"
 
